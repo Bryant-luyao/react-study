69528861cca4ac4adb9945a1b9271d6c4226fd27
huishunyong <774458182@qq.com>
2025-04-11 18:07:39 +0800

    feat: 新增日历组件

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,16 @@
+import { Dayjs } from 'dayjs'
+import MonthCalendar from './MonthCalendar'
+import Header from './Header'
+import './index.scss'
+
+export interface CalendarProps {
+    value: Dayjs
+}
+function Calendar(props: CalendarProps) {
+    return <div className="calendar">
+        <Header />
+        <MonthCalendar {...props}></MonthCalendar>
+    </div>
+}
+
+export default Calendar
